d9cad533b4e3ae018cb7578c03785e26f2af659b
Alessya.Krapivina <alessya.k00@gmail.com>
2023-08-29 00:25:35 -0400

    added initial functionality to select

--- src/app.js
+++ src/app.js
@@ -10,10 +10,29 @@ function displayCityData(city, timezone) {
 function update() {
   displayCityData("toronto", "America/Toronto");
   displayCityData("almaty", "Asia/Almaty");
-  displayCityData("newYork", "America/Toronto");
+  displayCityData("newYork", "America/New_York");
   displayCityData("berlin", "Europe/Berlin");
   displayCityData("london", "Europe/London");
 }
 
 update();
 setInterval(update, 1000);
+
+function updateCityData(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
+  let cities = document.querySelector("#cities");
+  cities.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
+let cities = document.querySelector("#cityOption");
+cities.addEventListener("change", updateCityData);
